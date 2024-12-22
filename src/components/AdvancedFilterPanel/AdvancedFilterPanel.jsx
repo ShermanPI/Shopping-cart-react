87bdb94782789265e71d9a54ea22e77f01a2d598
Sherman Perez <shermanperez0@gmail.com>
2024-12-21 23:10:10 -0400

    changed advanced filter styles

--- src/components/AdvancedFilterPanel/AdvancedFilterPanel.jsx
+++ src/components/AdvancedFilterPanel/AdvancedFilterPanel.jsx
@@ -1,42 +1,49 @@
 import './AdvancedFilterPanel.css'
 import FilterIcon from '../../assets/Icons/FilterIcon'
-import { useContext, useId, useState } from 'react'
+import { useContext, useEffect, useId, useRef, useState } from 'react'
 import { FiltersContext } from '../../contexts/FiltersContext'
 
 const AdvancedFilterPanel = () => {
   const [panelOpen, setPanelOpen] = useState(false)
   const { maxProductsPrice, setMinPrice, filters } = useContext(FiltersContext)
   const minPriceID = useId()
+  const panelOptionContainerRef = useRef()
+
+  useEffect(() => {
+    document.addEventListener('click', (e) => {
+      console.log(panelOpen)
+    })
+  }, [])
 
   return (
     <div className='all-filters-container'>
       <div onClick={() => setPanelOpen(!panelOpen)} className='filters-panel-btn'>
         <FilterIcon width={18} />
       </div>
-      {panelOpen &&
-        <div className='panel-options-container'>
-          <div className='form-field'>
-            <label htmlFor={minPriceID}>
-              Min Price:
-            </label>
-            <span className='price-range-container'>
-              <input
-                className='price-range'
-                id={minPriceID}
-                type='range'
-                name='volume'
-                step='10'
-                max={Math.floor(maxProductsPrice)}
-                value={filters.minPrice}
-                onChange={(value) => { setMinPrice(value.target.value) }}
-              />
-              <p className='price-range-number'>
-                $ {filters.minPrice}
-              </p>
-            </span>
+      <div className={`panel-options-container ${panelOpen ? 'open' : ''}`} ref={panelOptionContainerRef}>
+        <div className='form-field'>
+          <label htmlFor={minPriceID}>
+            Min Price:
+          </label>
+          <span className='price-range-container'>
+            <input
+              className='price-range'
+              id={minPriceID}
+              type='range'
+              name='volume'
+              step='10'
+              max={Math.floor(maxProductsPrice)}
+              value={filters.minPrice}
+              onChange={(value) => { setMinPrice(value.target.value) }}
+            />
+            <p className='price-range-number'>
+              $ {filters.minPrice}
+            </p>
+          </span>
 
-          </div>
-        </div>}
+        </div>
+
+      </div>
     </div>
 
   )
