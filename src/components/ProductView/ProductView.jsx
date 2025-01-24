4c6adf5f81d7498098767d778b5dd2533810373b
Sherman Perez <shermanperez0@gmail.com>
2025-01-23 23:49:20 -0400

    Add product description and enhance specifications layout in ProductView

--- src/components/ProductView/ProductView.jsx
+++ src/components/ProductView/ProductView.jsx
@@ -53,6 +53,10 @@ export const ProductView = () => {
                 <h2>{product?.title}</h2>
                 <RatingStars rating={product?.rating} />
                 <h2>${product?.price}</h2>
+
+                <h3>About this item</h3>
+                <p>{product?.description}</p>
+
                 <Button>
                   <ShoppingCart width={18} height={18} />
                   Add to cart
@@ -69,16 +73,16 @@ export const ProductView = () => {
                   <div className='product-specifications-info'>
                     <div className='product-specifications-info-column'>
                       <ul>
-                        <li>Brand</li>
-                        <li>Model</li>
-                        <li>Dimensions</li>
+                        <li><p>Brand</p></li>
+                        <li><p>Model</p></li>
+                        <li><p>Dimensions</p></li>
                       </ul>
                     </div>
                     <div className='product-specifications-info-column'>
                       <ul>
-                        <li>{product?.brand || ' '}</li>
-                        <li>{product?.model || 'N/A'}</li>
-                        <li>{product.dimensions.depth} x {product.dimensions.height} x {product.dimensions.width}</li>
+                        <li><p>{product?.brand || ' '}</p></li>
+                        <li><p>{product?.model || 'N/A'}</p></li>
+                        <li><p>{product.dimensions.depth} x {product.dimensions.height} x {product.dimensions.width}</p></li>
                       </ul>
                     </div>
                   </div>
