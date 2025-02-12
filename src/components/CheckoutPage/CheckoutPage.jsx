0a157b6b3938ae4a7291d83747d01004f5615355
Sherman Perez <shermanperez0@gmail.com>
2025-02-11 23:29:06 -0400

    Enhance CheckoutPage component; improve layout with grid styling, add order summary section, and update total price display.

--- src/components/CheckoutPage/CheckoutPage.jsx
+++ src/components/CheckoutPage/CheckoutPage.jsx
@@ -4,17 +4,57 @@ import './checkoutPage.css'
 export const CheckoutPage = () => {
   return (
     <section className='checkout-page-container'>
-      <h2 className='results-title'>Checkout</h2>
-      <section>
-        <div className='checkout-products' />
+      <h1 className='results-title'>Checkout</h1>
+      <section className='checkout-container'>
+        <div className='checkout-products white-checkout-container'>
+          <h2>Order Summary</h2>
+
+          <div className='checkout-product'>
+            <div>
+              <img src='' alt='' />
+            </div>
+            <div>
+              <p>name</p>
+              <p>$price</p>
+            </div>
+          </div>
+
+          <div className='checkout-product'>
+            <div>
+              <img src='' alt='' />
+            </div>
+            <div>
+              <p>name</p>
+              <p>$price</p>
+            </div>
+          </div>
+
+          <div className='checkout-product'>
+            <div>
+              <img src='' alt='' />
+            </div>
+            <div>
+              <p>name</p>
+              <p>$price</p>
+            </div>
+          </div>
+
+          <div className='checkout-total-price-container'>
+            <h3>Total:</h3>
+            <p>$1234</p>
+          </div>
+        </div>
         <div className='checkout-info-container'>
-          <div className='checkout-shipping-info'>
-            Shipping Address
+          <div className='checkout-shipping-info white-checkout-container'>
+            <h2>Order Summary</h2>
           </div>
-          <div className='checkout-payment-info'>
-            Payment Method
+          <div className='checkout-payment-info white-checkout-container'>
+            <h2>Payment Method</h2>
           </div>
-          <Button text='Place Order' />
+          <Button>
+            Place Order
+          </Button>
+
         </div>
       </section>
     </section>
