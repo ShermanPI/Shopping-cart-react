78d59442d74b5172d585b1374116b485f9ee30ef
Sherman Perez <shermanperez0@gmail.com>
2025-02-16 23:44:41 -0400

    Enhance CheckoutPage component; improve address selection UI, update styles, and display selected address information.

--- src/components/CheckoutPage/CheckoutPage.jsx
+++ src/components/CheckoutPage/CheckoutPage.jsx
@@ -1,16 +1,31 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import Button from '../Button/Button'
 import './checkoutPage.css'
 import { CartContext } from 'src/contexts/CartContext'
 
 export const CheckoutPage = () => {
+  const addressesOptions = [
+    {
+      id: 'some-id-123',
+      street: '123 Main St',
+      state: 'New York, 10001'
+    }, {
+      id: 'some-id-321',
+      street: '321 Not Main St',
+      state: 'Not new York, 10001'
+    }]
+
   const {
     cartItems,
     totalPrice
   } = useContext(CartContext)
+  const [selectedAddress, setSelectedAddress] = useState(addressesOptions[0].id)
+
+  const selectedOpitonInfo = addressesOptions.find(el => el.id === selectedAddress)
 
   return (
     <section className='checkout-page-container'>
+      {selectedAddress.id}
       <h1 className='results-title'>Checkout</h1>
       <section className='checkout-container'>
         <div className='checkout-products white-checkout-container'>
@@ -43,16 +58,32 @@ export const CheckoutPage = () => {
           <div className='checkout-shipping-info white-checkout-container checkout-container-padding'>
             <h2 className='checkout-container-subtitle'>Shipping Address</h2>
 
-            <select name='addresses' id='client-addresses'>
-              <option value='spider'>Spider</option>
-              <option value='goldfish'>Goldfish</option>
+            <select
+              name='addresses'
+              id='client-addresses'
+              onChange={(e) => {
+                setSelectedAddress(e.target.value)
+              }}
+            >
+              {addressesOptions.map(addressOption => {
+                return (
+                  <option
+                    value={addressOption.id}
+                    key={addressOption.id}
+                  >
+                    {addressOption.street}
+                  </option>
+                )
+              })}
             </select>
 
-            <p className='client-name'>John Doe</p>
-            <p>123 Main St</p>
-            <p>New York, 10001</p>
+            <div className='client-info'>
+              <p className='client-name'>John Doe</p>
+              <p>{selectedOpitonInfo.street}</p>
+              <p>{selectedOpitonInfo.state}</p>
+            </div>
 
-            <button className='checkout-white-btn'>
+            <button className='checkout-white-button'>
               Edit address
             </button>
           </div>
@@ -65,7 +96,7 @@ export const CheckoutPage = () => {
               <label><input type='radio' name='payment-option' value='2' /> MasterCard ending in 5678 </label>
             </div>
 
-            <button className='checkout-white-btn'>
+            <button className='checkout-white-button'>
               Add new payment method
             </button>
           </div>
