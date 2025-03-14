24d680463ec352e44728af856f67e3e989c26d93
Sherman Perez <shermanperez0@gmail.com>
2025-03-13 23:30:00 -0400

    Add advanced filter icons and layout to AdvancedFilterPanel

---
+++ src/assets/Icons/UpIcon.jsx
@@ -0,0 +1,15 @@
+const UpIcon = ({ width = 22, height = 22 }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 -960 960 960'
+      fill='currentColor'
+    >
+      <path d='M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z' />
+    </svg>
+  )
+}
+
+export default UpIcon
--- src/components/AdvancedFilterPanel/AdvancedFilterPanel.jsx
+++ src/components/AdvancedFilterPanel/AdvancedFilterPanel.jsx
@@ -1,11 +1,12 @@
 import './AdvancedFilterPanel.css'
 import FilterIcon from '../../assets/Icons/FilterIcon'
-import { useId, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
+import DollarIcon from 'src/assets/Icons/DollarIcon'
+import UpIcon from 'src/assets/Icons/UpIcon'
 
 const AdvancedFilterPanel = () => {
   const [panelOpen, setPanelOpen] = useState(false)
   // const { maxProductsPrice, setMinPrice, filters } = useContext(FiltersContext)
-  const minPriceID = useId()
   const panelOptionContainerRef = useRef()
 
   return (
@@ -14,25 +15,14 @@ const AdvancedFilterPanel = () => {
         <FilterIcon width={18} />
       </div>
       <div className={`panel-options-container ${panelOpen ? 'open' : ''}`} ref={panelOptionContainerRef}>
-        <div className='form-field'>
-          <label htmlFor={minPriceID}>
-            Min Price:
-          </label>
-          <span className='price-range-container'>
-            <input
-              className='price-range'
-              id={minPriceID}
-              type='range'
-              name='volume'
-              step='10'
-              // max={Math.floor(maxProductsPrice)}
-              // value={filters.minPrice}
-              // onChange={(value) => { setMinPrice(value.target.value) }}
-            />
-            <p className='price-range-number'>
-              {/* $ {filters.minPrice} */}
-            </p>
-          </span>
+        <div className='advanced-filter'>
+          <div className='advanced-filter-item'>
+            <DollarIcon />
+            PRICE
+          </div>
+          <div className='advanced-filter-icon-container'>
+            <UpIcon />
+          </div>
         </div>
       </div>
     </div>
